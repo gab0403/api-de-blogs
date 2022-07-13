f69dd055f9fd0eacbf4af9f4b57dee85680c8804
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-07-13 16:22:55 -0300

    fix: corrige o nome da coluna PostCategories e altera o nome da migration User para Users

--- src/database/migrations/20220713175222-PostCategories.js
+++ src/database/migrations/20220713175222-PostCategories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("PostsCategories", {
+    await queryInterface.createTable("PostCategories", {
       postId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -24,6 +24,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.dropTable("PostsCategories");
+    await queryInterface.dropTable("PostCategories");
   }
 };
